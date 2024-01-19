ad11df2f813cf8b2db4b77be479d4b5380507a86
maxirivadero <maxirivadero2000@gmail.com>
2024-01-19 15:47:17 -0300

    agregamos whatsapp

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,22 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FloatingWhatsappComponent } from './components/floating-whatsapp/floating-whatsapp.component';
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    FloatingWhatsappComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    FloatingWhatsappComponent,
+    
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA  ]
 })
