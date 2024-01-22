1504ecf4eed9622b0f64ef26fc1398eb34858eac
maxirivadero <maxirivadero2000@gmail.com>
2024-01-21 18:40:59 -0600

    navbar no funcional

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  isSideMenuVisible = false;
 
+  constructor(private renderer: Renderer2) {}
+
+  toggleSideMenu() {
+    this.isSideMenuVisible = !this.isSideMenuVisible;
+
+    // Obtén el elemento de la clase side-menu y ajusta su estilo de visualización
+    const sideMenu = document.querySelector('.side-menu');
+    if (sideMenu) {
+      if (this.isSideMenuVisible) {
+        this.renderer.addClass(sideMenu, 'active');
+      } else {
+        this.renderer.removeClass(sideMenu, 'active');
+      }
+    }
+  }
 }
