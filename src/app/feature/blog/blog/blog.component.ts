22d17f4e726ad934cf20107587e2068a59ef7a79
maxirivadero <maxirivadero2000@gmail.com>
2024-02-01 20:42:18 -0600

    ya estan los estilos basicos, se pueden mejorar

--- src/app/feature/blog/blog/blog.component.ts
+++ src/app/feature/blog/blog/blog.component.ts
@@ -13,19 +13,19 @@ export class BlogComponent {
   };
   blogsData = [
     { 
-        imageSrc: '../../../../assets/images/camion-1.jpeg',
+        imageSrc: '../../../../assets/images/blog/dador-carga.jpg',
         title: 'Dador de carga',
         description: 'blog1-p',
         link: '/blog/dador-de-carga'
     },
     { 
-        imageSrc: '../../../../assets/images/camion-2.jpeg',
+        imageSrc: '../../../../assets/images/blog/seguimiento.jpg',
         title: 'Seguimiento',
         description: 'blog2-p',
         link: '/blog/seguimiento'
     },
     { 
-        imageSrc: '../../../../assets/images/camion-3.jpeg',
+        imageSrc: '../../../../assets/images/blog/tercerizar-cargas.jpg',
         title: 'Tercerizar Cargas',
         description: 'blog3-p',
         link: '/blog/tercerizar-cargas'
