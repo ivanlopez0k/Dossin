9f1bee7b65a0fcd1244c0e9479b8d8bb9e190b4b
maxirivadero <maxirivadero2000@gmail.com>
2024-02-04 03:54:25 -0600

    empeze algo de viajes v2

--- src/app/feature/viajes/viajes/viajes.component.ts
+++ src/app/feature/viajes/viajes/viajes.component.ts
@@ -10,15 +10,46 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ViajesComponent implements OnInit{
   datosViaje:FormGroup;
+  datosEmpresa:FormGroup;
+  datosChofer:FormGroup;
+  datosCamion:FormGroup;
+
   constructor(private _formBuilder: FormBuilder, private translate: TranslateService){
     this.datosViaje = this._formBuilder.group({
-      
       tipoForm: 'Viajes',
       nombre: new FormControl('', Validators.required),
       apellido: new FormControl('', Validators.required),
       telefono: new FormControl('', Validators.required),
       transporte: new FormControl('', Validators.required),
       mensaje: new FormControl('', Validators.required)
+    })
+
+    this.datosEmpresa = this._formBuilder.group({
+      tipoForm: 'Empresa',
+      ahaAfip: new FormControl('', Validators.required),
+      libreDeuda: new FormControl('', Validators.required),
+      constanciaIIBB: new FormControl('', Validators.required),
+      constanciaCBU: new FormControl('', Validators.required)
+    })
+
+    this.datosChofer = this._formBuilder.group({
+      tipoForm: 'Chofer',
+      dni: new FormControl('', Validators.required),
+      licencia: new FormControl('', Validators.required),
+      altaTemprana: new FormControl('', Validators.required),
+      art: new FormControl('', Validators.required),
+      linti: new FormControl('', Validators.required),
+      seguroVida: new FormControl('', Validators.required)
+  
+    })
+
+    this.datosCamion = this._formBuilder.group({
+      tipoForm: 'Camion',
+      rto: new FormControl('', Validators.required),
+      polizaSeguro: new FormControl('', Validators.required),
+      pasoSeguro: new FormControl('', Validators.required),
+      ruta: new FormControl('', Validators.required),
+      certificadoSatelital: new FormControl('', Validators.required)
     })
   }
 
@@ -30,8 +61,35 @@ export class ViajesComponent implements OnInit{
     return this.datosViaje.controls[formControlName];
   }
   
+  getEmpresaControl(formControlName: string) {
+    return this.datosEmpresa.controls[formControlName];
+  }
+
+  getChoferControl(formControlName: string) {
+    return this.datosChofer.controls[formControlName];
+  }
+  
+  getCamionControl(formControlName: string) {
+    return this.datosCamion.controls[formControlName];
+  }
+
   isFormControlInvalid(formControlName: string): boolean {
     const control = this.getControl(formControlName);
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  isEmpresaInvalid(formControlName: string): boolean {
+    const control = this.getEmpresaControl(formControlName);
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  isChoferInvalid(formControlName: string): boolean {
+    const control = this.getChoferControl(formControlName);
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  isCamionInvalid(formControlName: string): boolean {
+    const control = this.getCamionControl(formControlName);
     return control.invalid && (control.dirty || control.touched);
   }
 
