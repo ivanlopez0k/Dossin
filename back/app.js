f773c72d01436d78cdd56515ba071f0af1634db2
Ivan <ivanlopezokk@gmail.com>
2024-02-16 03:06:53 -0300

    prueba de email+back

--- back/app.js
+++ back/app.js
@@ -1,34 +1,82 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis')
+const { google } = require('googleapis');
+const cors = require('cors');
 const fs = require('fs');
 const viajesContent = fs.readFileSync('./viajesContent.html', 'utf-8');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage,limits: { fileSize: 1024 * 1024 * 10 } });
+
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 
 
+app.use((req, res, next) => {
+  console.log(req.body); 
+  next();
+});
 
-app.post('/viajes', async (req, res) => {
+app.post('/viajes', upload.array('archivosPDF'), async (req, res) => {
+  console.log('Archivos recibidos:', req.files);
+  console.log('Datos del formulario:', req.body);
+const { nombre, apellido, telefono, transporte, ahaAfipEmpresa, libreDeudaEmpresa, constanciaIIBBEmpresa, constanciaCBUEmpresa, dniChofer, licenciaChofer, altaTempranaChofer, artChofer, lintiChofer, seguroVidaChofer, rtoCamion, polizaSeguroCamion, pasoSeguroCamion, rutaCamion,certificadoSatelitalCamion } = req.body;
+const archivosPDF = req.files;
 
+console.log('Datos recibidos en el backend:', req.body);
+console.log('Datos del formulario:', nombre);
 
-  
     const  email  = 'shapening7@gmail.com';
     const  CLIENT_ID  = '818969776008-l686f5a6lfr1hhe9i4h84jhgvefkk0na.apps.googleusercontent.com';
     const  CLIENT_SECRET  = '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';
     const  REDIRECT_URI  = 'https://developers.google.com/oauthplayground';
     const REFRESH_TOKEN  = '1//04Dhk_SU3Ffn2CgYIARAAGAQSNwF-L9IrqyLScE5x6zZFh_fekPJ6ghBBo5gnWtOj4qwWE2VYt0e7Jn9_c9W82kJqXBh-v1nVXMo';
-  
+
     const oAuth2Client = new google.auth.OAuth2(
       CLIENT_ID,
       CLIENT_SECRET,
       REDIRECT_URI,
     );
-  
+    const contenidoModificado = viajesContent.replace('{{nombre}}', nombre)
+    .replace('{{apellido}}', apellido)
+    .replace('{{telefono}}', telefono)
+    .replace('{{transporte}}', transporte)
+    .replace('{{ahaAfipEmpresa}}', ahaAfipEmpresa)
+    .replace('{{libreDeudaEmpresa}}', libreDeudaEmpresa)
+    .replace('{{constanciaIIBBEmpresa}}', constanciaIIBBEmpresa)
+    .replace('{{constanciaCBUEmpresa}}', constanciaCBUEmpresa)
+    .replace('{{dniChofer}}', dniChofer)
+    .replace('{{licenciaChofer}}', licenciaChofer)
+    .replace('{{altaTempranaChofer}}', altaTempranaChofer)
+    .replace('{{artChofer}}', artChofer)
+    .replace('{{lintiChofer}}', lintiChofer)
+    .replace('{{rtoCamion}}', rtoCamion)
+    .replace('{{polizaSeguroCamion}}', polizaSeguroCamion)
+    .replace('{{pasoSeguroCamion}}', pasoSeguroCamion)
+    .replace('{{rutaCamion}}', rutaCamion)
+    .replace('{{certificadoSatelitalCamion}}', certificadoSatelitalCamion)
+    .replace('{{seguroVidaChofer}}', seguroVidaChofer)
+
+
     oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
   
   
     async function sendMail() {
+      archivosPDF.forEach((archivo, index) => {
+});
       try {
         const tokenAcceso = await oAuth2Client.getAccessToken();
         const transporter = nodemailer.createTransport({
@@ -42,11 +90,18 @@ app.post('/viajes', async (req, res) => {
             accessToken: tokenAcceso,
           },
         });
+
+        const attachments = archivosPDF.map((archivo, index) => ({
+          filename: archivo.originalname,
+          content: archivo.buffer,
+        }));
+
         const mailOptions = {
           from: 'shapening7@gmail.com',
           to: email,
           subject: 'Prueba de Viajes',
-          html: viajesContent,
+          html: contenidoModificado,
+          attachments: attachments
         };
         const info = await transporter.sendMail(mailOptions);
         console.log('Correo electrónico enviado:', info.response);
@@ -56,9 +111,8 @@ app.post('/viajes', async (req, res) => {
         return res.status(500).json({ error: 'Error interno al enviar el correo' });
       }
     }
-    sendMail()
-      .then(result => res.status(200).send('enviado'))
-      .catch((error) => console.log(error.message));
+    sendMail(nombre, archivosPDF)
+
   });
   
 
