88e559bceeb24a04ddb8db54e86f1e1bd740aee9
maxirivadero <maxirivadero2000@gmail.com>
2024-02-19 01:49:07 -0300

    arreglado los errores en el form de viajes

--- front/src/app/feature/viajes/viajes/viajes.component.ts
+++ front/src/app/feature/viajes/viajes/viajes.component.ts
@@ -10,9 +10,41 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ViajesComponent implements OnInit {
   datosViaje: FormGroup;
-  archivoLabelText: string = '';
-  selectedFile: File[] | any | null = [];
+  
+  archivosPorCampo: { [campo: string]: File[] } = {
+    ahaAfipEmpresa: [],
+    libreDeudaEmpresa: [],
+    constanciaIIBBEmpresa: [],
+    constanciaCBUEmpresa: [],
+    dniChofer: [],
+    licenciaChofer: [],
+    altaTempranaChofer: [],
+    artChofer: [],
+    lintiChofer: [],
+    seguroVidaChofer: [],
+    rtoCamion: [],
+    polizaSeguroCamion: [],
+    pasoSeguroCamion: [],
+    rutaCamion: [],
+    certificadoSatelitalCamion: [],
+  };  
+  archivoLabelTextAfip: string = '';
+  archivoLabelTextLibreDeuda: string = '';
+  archivoLabelTextIIBB: string = '';
+  archivoLabelTextCBU: string = '';
+  archivoLabelTextDniChofer: string = '';
+  archivoLabelTextLicenciaChofer: string = '';
+  archivoLabelTextAltaTempranaChofer: string = '';
+  archivoLabelTextArtChofer: string = '';
+  archivoLabelTextLintiChofer: string = '';
+  archivoLabelTextSeguroVidaChofer: string = '';
+  archivoLabelTextRtoCamion: string = '';
+  archivoLabelTextPolizaSeguroCamion: string = '';
+  archivoLabelTextPasoSeguroCamion: string = '';
+  archivoLabelTextRutaCamion: string = '';
+  archivoLabelTextCertificadoSatelitalCamion: string = '';
 
+  cargando: boolean = false;
   constructor(private _formBuilder: FormBuilder,private archivoService: ArchivoServiceService, private translate: TranslateService) {
     this.datosViaje = this._formBuilder.group({
       tipoForm: 'Viajes',
@@ -58,37 +90,48 @@ export class ViajesComponent implements OnInit {
       certificadoSatelitalArchivo: new FormControl(''),
     });
 
-    this.setupFileControl('ahaAfipEmpresa', 'ahaAfipArchivo');
-    this.setupFileControl('libreDeudaEmpresa', 'libreDeudaArchivo');
-    this.setupFileControl('constanciaIIBBEmpresa', 'constanciaIIBBArchivo');
-    this.setupFileControl('constanciaCBUEmpresa', 'constanciaCBUArchivo');
-    this.setupFileControl('dniChofer', 'dniChoferArchivo');
-    this.setupFileControl('licenciaChofer', 'licenciaChoferArchivo');
-    this.setupFileControl('altaTempranaChofer', 'altaTempranaChoferArchivo');
-    this.setupFileControl('artChofer', 'artChoferArchivo');
-    this.setupFileControl('lintiChofer', 'lintiChoferArchivo');
-    this.setupFileControl('seguroVidaChofer', 'seguroVidaChoferArchivo');
-    this.setupFileControl('rtoCamion', 'rtoArchivo');
-    this.setupFileControl('polizaSeguroCamion', 'polizaSeguroArchivo');
-    this.setupFileControl('pasoSeguroCamion', 'pasoSeguroArchivo');
-    this.setupFileControl('rutaCamion', 'rutaCamionArchivo');
-    this.setupFileControl('certificadoSatelitalCamion', 'certificadoSatelitalArchivo');
+    this.setupFileControl('ahaAfipEmpresa', 'ahaAfipArchivo', 'archivoLabelTextAfip');
+    this.setupFileControl('libreDeudaEmpresa', 'libreDeudaArchivo', 'archivoLabelTextLibreDeuda');
+    this.setupFileControl('constanciaIIBBEmpresa', 'constanciaIIBBArchivo', 'archivoLabelTextIIBB');
+    this.setupFileControl('constanciaCBUEmpresa', 'constanciaCBUArchivo', 'archivoLabelTextCBU');
+    this.setupFileControl('dniChofer', 'dniChoferArchivo', 'archivoLabelTextDniChofer');
+    this.setupFileControl('licenciaChofer', 'licenciaChoferArchivo', 'archivoLabelTextLicenciaChofer');
+    this.setupFileControl('altaTempranaChofer', 'altaTempranaChoferArchivo', 'archivoLabelTextAltaTempranaChofer');
+    this.setupFileControl('artChofer', 'artChoferArchivo', 'archivoLabelTextArtChofer');
+    this.setupFileControl('lintiChofer', 'lintiChoferArchivo', 'archivoLabelTextLintiChofer');
+    this.setupFileControl('seguroVidaChofer', 'seguroVidaChoferArchivo', 'archivoLabelTextSeguroVidaChofer');
+    this.setupFileControl('rtoCamion', 'rtoArchivo', 'archivoLabelTextRtoCamion');
+    this.setupFileControl('polizaSeguroCamion', 'polizaSeguroArchivo', 'archivoLabelTextPolizaSeguroCamion');
+    this.setupFileControl('pasoSeguroCamion', 'pasoSeguroArchivo', 'archivoLabelTextPasoSeguroCamion');
+    this.setupFileControl('rutaCamion', 'rutaCamionArchivo', 'archivoLabelTextRutaCamion');
+    this.setupFileControl('certificadoSatelitalCamion', 'certificadoSatelitalArchivo', 'archivoLabelTextCertificadoSatelitalCamion');
+    
   }
 
 
-  setupFileControl(triggerControlName: string, targetControlName: string): void {
-    this.datosViaje.get(triggerControlName)?.valueChanges.subscribe(value => {
-      const targetControl = this.datosViaje.get(targetControlName);
-
-      if (value === 'Si') {
+setupFileControl(triggerControlName: string, targetControlName: string, archivoLabelTextVariable: string): void {
+  this.datosViaje.get(triggerControlName)?.valueChanges.subscribe(value => {
+    const targetControl = this.datosViaje.get(targetControlName);
+    const campoPrefix = triggerControlName.includes('Empresa') ? 'Empresa' : (triggerControlName.includes('Chofer') ? 'Chofer' : 'Camion');
+    if (value === 'Si') {
+      // Si se selecciona "Si", validar si hay archivos subidos
+      const archivosSubidos = this.archivosPorCampo[campoPrefix].length > 0;
+    
+      if (!archivosSubidos) {
         targetControl?.setValidators([Validators.required]);
       } else {
         targetControl?.clearValidators();
       }
-
-      targetControl?.updateValueAndValidity();
-    });
-  }
+    } else {
+      // Si se selecciona "No", limpiar validadores y archivos
+      targetControl?.clearValidators();
+      this.eliminarArchivos(targetControlName, archivoLabelTextVariable);
+    }
+    
+    targetControl?.updateValueAndValidity();
+  });
+}
+  
 
   
 
@@ -105,39 +148,94 @@ export class ViajesComponent implements OnInit {
 
   async submitForm() {
     if (this.datosViaje.valid) {
-
-      const archivosPDF = this.selectedFile;
-
-
+      this.cargando = true; // Inicia la señal de carga
+  
+      // Extraer todos los archivos en un solo array
+      const archivosPDF = Object.values(this.archivosPorCampo).flat();
+      
       const datosFormulario = this.datosViaje.value;
-      console.log(datosFormulario)
-      this.archivoService.enviarArchivosYDatos( datosFormulario, archivosPDF )
-      .subscribe(
-        (respuesta) => {
-          console.log('Éxito:', respuesta);
-          alert('Mensaje enviado');
-          this.datosViaje.reset();
-        },
-        (error) => {
-          console.error('Error:', error);
-          alert('Error al enviar el mensaje');
-        }
-      );
+      console.log(datosFormulario);
+  
+      this.archivoService.enviarArchivosYDatos(datosFormulario, archivosPDF)
+        .subscribe(
+          (respuesta) => {
+            console.log('Éxito:', respuesta);
+            alert('Mensaje enviado');
+  
+            // Resetear el formulario
+            this.datosViaje.reset();
+  
+            // Limpiar archivosPorCampo y archivoLabelText
+            this.limpiarArchivosYLabels();
+          },
+          (error) => {
+            console.error('Error:', error);
+            alert('Error al enviar el mensaje');
+          }
+        )
+        .add(() => {
+          this.cargando = false; // Finaliza la señal de carga, independientemente del éxito o error
+        });
     } else {
       this.datosViaje.markAllAsTouched();
     }
   }
-
-  onFileSelected(event: any): void {
-    const archivoInput = event.target as HTMLInputElement;
-
-    if (archivoInput?.files?.length) {
-      // Agregar los nuevos archivos a la lista existente
-      this.selectedFile = this.selectedFile.concat(Array.from(archivoInput.files));
-      this.archivoLabelText = this.selectedFile.map((file: { name: any; }) => file.name).join(', ');
-    } else {
-      this.selectedFile = [];
-      this.archivoLabelText = '';
+  
+  
+  limpiarArchivosYLabels(): void {
+    // Limpiar archivosPorCampo
+    for (const key in this.archivosPorCampo) {
+      if (this.archivosPorCampo.hasOwnProperty(key)) {
+        this.archivosPorCampo[key] = [];
+      }
+    }
+  
+    // Limpiar archivoLabelText
+    for (const key in this) {
+      if (this.hasOwnProperty(key) && key.startsWith('archivoLabelText')) {
+        (this as Record<string, any>)[key] = '';
+      }
     }
   }
+
+  onFileSelected(event: any, archivoLabelTextVariable: string, campo: string): void {
+    const archivoInput = event.target as HTMLInputElement;
+  
+    if (archivoInput?.files?.length) {
+      const selectedFiles = Array.from(archivoInput.files);
+  
+      // Almacenar los archivos en el arreglo correspondiente al campo
+      this.archivosPorCampo[campo] = (this.archivosPorCampo[campo] || []).concat(selectedFiles);
+  
+      // Actualizar la labelText con los nombres de los archivos
+      const existingFiles = this.archivosPorCampo[campo].map((file: { name: any; }) => file.name).join(', ');
+      (this as any)[archivoLabelTextVariable] = existingFiles ? existingFiles + ', ' : '';
+  
+      // Actualizar el valor del control en el formulario
+      const archivoControl = this.datosViaje.get(`${campo.replace('Empresa', '')}Archivo`);
+      if (archivoControl) {
+        archivoControl.setValue(this.archivosPorCampo[campo]);
+      }
+    }
+  }
+  
+eliminarArchivos(campo: string, archivoLabelTextVariable: string): void {
+  // Limpiar el arreglo de archivos correspondiente al campo
+  this.archivosPorCampo[campo] = [];
+
+  // Limpiar el valor del control en el formulario
+  const archivoControl = this.datosViaje.get(`${campo.replace('Empresa', '')}Archivo`);
+  if (archivoControl) {
+    archivoControl.setValue([]);
+  }
+
+  // Limpiar la labelText
+  (this as any)[archivoLabelTextVariable] = '';
 }
+  
+
+  
+}
+//---------------------------
+
+//-----------------------------
